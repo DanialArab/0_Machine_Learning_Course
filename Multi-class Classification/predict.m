1882bfee07cc60be489054030d1b81a591dd6a71
Danial Arab <54812742+DanialArab@users.noreply.github.com>
2021-10-25 11:48:21 -0700

    Update predict.m

--- Multi-class Classification/predict.m
+++ Multi-class Classification/predict.m
@@ -6,13 +6,13 @@ num_labels = size(Theta2, 1);
 p = zeros(size(X, 1), 1);
 
 
-a1 = [ones(m, 1) X];
-z2 = a1 * Theta1';
-a2 = [ones(size(z2, 1), 1) sigmoid(z2)];
-z3 = a2*Theta2';
-a3 = sigmoid(z3);
+activation1 = [ones(m, 1) X];
+z_2 = activation1 * Theta1';
+activation2 = [ones(size(z_2, 1), 1) sigmoid(z_2)];
+z_3 = activation2 * Theta2';
+activation3 = sigmoid(z_3);
 
-[p_max, p] = max(a3, [], 2);
+[p_max, p] = max(activation3, [], 2);
 
 
 end
